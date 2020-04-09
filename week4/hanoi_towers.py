91a94a6a5d80145a2cd792f411c41f11bf3a53c9
NK-Natalia <57593042+NK-Natalia@users.noreply.github.com>
2020-04-09 12:42:51 +0200

    add Hanoi Tower Puzzle Solution

---
+++ week4/hanoi_towers.py
@@ -0,0 +1,15 @@
+# Hanoi Tower Puzzle Solution
+
+
+def move(n, x, y):
+    if n == 1:
+        print(n, x, y)
+    else:
+        tmp = (6 - x) - y
+        move((n - 1), x, tmp)
+        print(n, x, y)
+        move((n - 1), tmp, y)
+
+
+n = int(input())
+move(n, 1, 3)
