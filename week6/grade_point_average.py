cfab679dacd969dc000e94f4587ef7e5548f954a
NK-Natalia <57593042+NK-Natalia@users.noreply.github.com>
2020-04-24 14:46:19 +0200

    add count average grade point from extern file

---
+++ week6/grade_point_average.py
@@ -0,0 +1,14 @@
+import statistics as st
+inFile = open('./input.txt', 'r', encoding='utf8')
+list9 = []
+list10 = []
+list11 = []
+for line in inFile:
+    if int(line.split()[2]) == 9:
+        list9.append(int(line.split()[3]))
+    elif int(line.split()[2]) == 10:
+        list10.append(int(line.split()[3]))
+    elif int(line.split()[2]) == 11:
+        list11.append(int(line.split()[3]))
+
+print(st.mean(list9), st.mean(list10), st.mean(list11), end=' ')
