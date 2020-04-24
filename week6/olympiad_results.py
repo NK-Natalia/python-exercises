48d2eb6f07e5794d77ec5cdc071e53c2563c85d7
NK-Natalia <57593042+NK-Natalia@users.noreply.github.com>
2020-04-24 22:47:01 +0200

    add arrange the list in descending order of scores

--- week6/olympiad_results.py
+++ week6/olympiad_results.py
@@ -1,14 +1,14 @@
 # Arrange the list of participants in the Olympiad
 # in descending order of scores.
-
-inFile = open('input.txt', 'r', encoding='utf8')
-
-for i in 
-lines = inFile.readlines()
-
-lines.sort()
-for line in lines:
-    line = (line.split())
-    line.pop(2)
-    print(*line, file=outFile)
-
+with open('input.txt', 'r', encoding='utf8') as inFile:
+    for lines in inFile:
+        lines = inFile.readlines()
+        newList = []
+        for line in lines:
+            name = (line.split())[0]
+            result = (line.split())[1]
+            newList.append((int(result), name))
+        newList.sort()
+        newList.reverse()
+        for new in newList:
+            print(new[1])
