20a44ead86c4c927218f0a4f4de78a88f5d8b8f3
NK-Natalia <57593042+NK-Natalia@users.noreply.github.com>
2020-05-06 21:42:19 +0200

    add new version

--- week7/Election_of_Deputies.py
+++ week7/Election_of_Deputies.py
@@ -1,30 +1,46 @@
-with open('input.txt') as inFile:
-    nameDict = {}
-    voteList = []
-    for line in inFile:
-        name = str(list(line.split())[:-1])
-        vote = list(line.split())[-1]
-        nameDict[vote] = name
-        voteList.append(int(vote))
-    print(nameDict)
-    print(voteList)
-    firstNumber = sum(voteList) / 450
-    print(firstNumber)
-    voteListN = []
-    voteListNfraction = []
-    voteDictN = {}
-    for vote in voteList:
-        x1 = vote // firstNumber
-        voteListN.append(x1)
-        voteListNfraction.append(vote % firstNumber)
-        voteDictN[x1] = nameDict[str(vote)]
-    print(voteListN)
-    print(voteDictN)
-    print(voteListNfraction)
-    controlNumber = sum(voteListN)
-    if controlNumber == 450:
-        print(voteDictN[x1], x1)
-    print(controlNumber)
+names = list()
+votes = list()
+sumVotes = 0
+inFile = open('input.txt')
+for line in inFile:
+    if len(line) < 2:
+        continue
     else:
-        for vote in votelist:
+        line = line.split()
+        partyName = ' '.join(line[:-1])
+        partyVotes = int(line[-1])
+        names.append(partyName)
+        votes.append(partyVotes)
+        sumVotes += partyVotes
+inFile.close()
 
+mandates = list()
+fracPart = []
+sumMandates = 0
+for i in range(len(names)):
+    partyMandates = (votes[i] * 450) / sumVotes
+    sumMandates += int(partyMandates)
+    mandates.append(int(partyMandates))
+    fracPart.append(partyMandates - int(partyMandates))
+
+# fracPart = enumerate(fracPart)
+# , key=lambda x: (x[1], x[0]), reverse=True)
+# print(mandates)
+while sumMandates < 450:
+    i = 0
+    for j in range(1, len(fracPart)):
+        if (
+            (fracPart[j] > fracPart[i]) or  # если следующий остаток больше
+            (fracPart[j] == fracPart[i] and votes[j] > votes[i])
+                ):  # если пред и след остатки равны, но след голосов больше
+            i = j
+    mandates[i] += 1
+    sumMandates += 1
+    fracPart[i] = 0
+
+for k in range(len(names)):
+    print(names[k], mandates[k])
+
+
+for k in range(len(names)):
+    print(names[k], mandates[k])
